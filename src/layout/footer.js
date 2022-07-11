8b96271b1fa0a8fca24fed532db0630d31117048
David Madrid <davidmadridpathway@gmail.com>
2022-07-11 11:30:23 -0500

    Footer styles finished

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -4,16 +4,16 @@ function Footer() {
   return (
     <div className='font-serif pt-20 p-10 space-y-20 bg-black text-white'>
       <div className='flex justify-center space-x-7 md:space-x-10'>
-        <NavLink className='hover:scale-125 duration-100' to='/'>World</NavLink>
-        <NavLink className='hover:scale-125 duration-100' to='/economy'>Economy</NavLink>
-        <NavLink className='hover:scale-125 duration-100' to='/politics'>Politics</NavLink>
-        <NavLink className='hover:scale-125 duration-100' to='/fashion'>Fashion</NavLink>
-        <NavLink className='hover:scale-125 duration-100' to='/sports'>Sports</NavLink>
+        <NavLink className='hover:scale-110 duration-100' to='/'>World</NavLink>
+        <NavLink className='hover:scale-110 duration-100' to='/economy'>Economy</NavLink>
+        <NavLink className='hover:scale-110 duration-100' to='/politics'>Politics</NavLink>
+        <NavLink className='hover:scale-110 duration-100' to='/fashion'>Fashion</NavLink>
+        <NavLink className='hover:scale-110 duration-100' to='/sports'>Sports</NavLink>
       </div>
       <div className='flex justify-center md:justify-end space-x-5'>
-        <a rel='noreferrer' target='_blank' href="https://github.com/DavidMadridYepez"><img src="./images/github (1).png" alt="" /></a>
-        <a rel='noreferrer' target='_blank' href="https://porfolio-v1-pearl.vercel.app/"><img src="./images/briefcase.png" alt="" /></a>
-        <a rel='noreferrer' target='_blank' href="https://www.linkedin.com/in/david-madrid-0809211a1/"><img src="./images/linkedin (1).png" alt="" /></a>
+        <a className='w-7' rel='noreferrer' target='_blank' href="https://github.com/DavidMadridYepez"><img src="./images/github (1).png" alt="" /></a>
+        <a className='w-7' rel='noreferrer' target='_blank' href="https://porfolio-v1-pearl.vercel.app/"><img src="./images/briefcase.png" alt="" /></a>
+        <a className='w-7' rel='noreferrer' target='_blank' href="https://www.linkedin.com/in/david-madrid-0809211a1/"><img src="./images/linkedin (1).png" alt="" /></a>
       </div>
     </div>
   )
